4fba07228841e5ecd1c28c30eb1c1d15882c430e
natsunohiori <natsunohiori@gmail.com>
2026-04-11 02:58:25 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-```tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -8,4 +7,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 );
-```
